a19377f94052ff9dc5079771a1d8584d741fe52c
gautam <gautamjuneja03@gmail.com>
2023-07-10 21:07:18 +0530

    Movies display by categories done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Constants
 const apikey = "e7390669ef1be2bc20b2eab76226bda8"
 const apiEndpoint = "https://api.themoviedb.org/3"
+const imgPath = "https://image.tmdb.org/t/p/original";
 
 const apiPaths = {
     fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
@@ -19,7 +20,7 @@ function fetchAndBuildAllSections(){
     .then(res => {
         const categories = res.genres;
         if(Array.isArray(categories) && categories.length){
-            categories.slice(0,2).forEach(category => {
+            categories.forEach(category => {
                 fetchAndBuildMovieSection(
                     apiPaths.fetchMoviesList(category.id),category
                 );
@@ -35,10 +36,46 @@ function fetchAndBuildMovieSection(fetchUrl,category){
     fetch(fetchUrl)
     .then(res => res.json())
     .then(res => {
-        console.table(res.results);
+        // console.table(res.results);
+        const movies = res.results;
+        if(Array.isArray(movies) && movies.length){
+            buildMoviesSection(movies, category.name);
+        }
     })
     .catch(err => console.error(err))
 }
+
+function buildMoviesSection(list,categoryName){
+    console.log(list,categoryName);
+    const moviesCont = document.getElementById('movies-cont');
+    const moviesListHTML = list.map(item => {
+        return `
+        <img class="movie-item" src="${imgPath}${item.backdrop_path}" alt="${item.title}">
+        `;
+    }).join('');
+
+    const moviesSectionHTML = `
+    
+        <h2 class="movie-section-heading">${categoryName}<span class="explore-nudge">Explore All</span></h2>
+        <div class="movies-row">
+            ${moviesListHTML}
+        </div>
+    
+    `
+    // console.log(moviesSectionHTML);
+
+    const div = document.createElement('div');
+    div.className="movies-section"
+    div.innerHTML = moviesSectionHTML;
+
+    // append HTML into container
+    moviesCont.append(div);
+}
+
+
+
+
+
 
 window.addEventListener('load',function(){
     init();
