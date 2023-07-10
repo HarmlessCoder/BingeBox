51fd90cea97cf78e0be0213d8484ccac05ea4e30
gautam <gautamjuneja03@gmail.com>
2023-07-10 19:55:49 +0530

    Data loading done ui left

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const apikey = "e7390669ef1be2bc20b2eab76226bda8"
 const apiEndpoint = "https://api.themoviedb.org/3"
 
 const apiPaths = {
-    fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`
+    fetchAllCategories: `${apiEndpoint}/genre/movie/list?api_key=${apikey}`,
+    fetchMoviesList : (id) => `${apiEndpoint}/discover/movie?api_key=${apikey}&with_genres=${id}` 
 }
 
 // Boots up the app
@@ -18,18 +19,25 @@ function fetchAndBuildAllSections(){
     .then(res => {
         const categories = res.genres;
         if(Array.isArray(categories) && categories.length){
-            categories.forEach(category => {
-                fetchAndBuildMovieSection(category);
+            categories.slice(0,2).forEach(category => {
+                fetchAndBuildMovieSection(
+                    apiPaths.fetchMoviesList(category.id),category
+                );
             });
-        };
-        console.table(movies);
+        }
+        // console.table(movies);
     })
     .catch(err=>console.error(err));
 }
 
-function fetchAndBuildMovieSection(category){
-    console.log(category);
-
+function fetchAndBuildMovieSection(fetchUrl,category){
+    console.log(fetchUrl,category);
+    fetch(fetchUrl)
+    .then(res => res.json())
+    .then(res => {
+        console.table(res.results);
+    })
+    .catch(err => console.error(err))
 }
 
 window.addEventListener('load',function(){
